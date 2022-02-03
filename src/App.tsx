43cd42e9eb0c94f9a2c7adbf4a663eebbc4367e6
sangminlee98 <tkdals0975@naver.com>
2022-02-03 17:19:38 +0900

    add NEWS API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState();
+  const onClick = async () => {
+    try {
+      const result = await axios(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${process.env.REACT_APP_API_KEY}`);
+      setData(result.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly/>}
     </div>
   );
 }
